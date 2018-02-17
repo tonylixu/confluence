ce7cf87006945fc269071659a93773508e9f8447
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-17 13:58:45 -0500

    Added import

--- groovy/page permission/create-page-restriction-goups.groovy
+++ groovy/page permission/create-page-restriction-goups.groovy
@@ -1,2 +1,12 @@
 // CNF version 6.5.0
 // Script will add defined groups to page restriction on page create.
+
+import com.atlassian.confluence.event.events.content.page.PageEvent
+import com.atlassian.confluence.core.ContentPermissionManager
+import com.atlassian.confluence.core.DefaultContentPermissionManager
+import com.atlassian.confluence.security.ContentPermission
+import com.atlassian.sal.api.component.ComponentLocator
+import com.atlassian.confluence.user.UserAccessor
+import com.atlassian.user.GroupManager
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
