b4e6fb35f0707d5158e19c2eee2c4f0cb28d310c
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-27 20:36:10 -0500

    Going through each space, restric all the pages that doesn't have view permission

--- groovy/page permission/update-all-space-page-permission.groovy
+++ groovy/page permission/update-all-space-page-permission.groovy
@@ -13,3 +13,33 @@ def pageManager = ComponentLocator.getComponent(PageManager)
 
 // Retrieve all the spaces
 def spaces = spaceManager.allSpaces.findAll()
+
+for (space in spaces) {
+    space_name = space['key']
+    log.info("spaces is " + space_name)
+	// Retrieve space
+	def space = spaceManager.getSpace(space_name)
+	// Retrieve all the pages in a given space
+	def pages = pageManager.getPages(space, true).findAll{it instanceof Page}
+	for (page in pages) {
+	    log.info("process page: " + page.title)
+		// Check if the page has view permission
+		ContentPermissionSet viewPermit = page
+                    .getContentPermissionSet(ContentPermission.VIEW_PERMISSION);
+		log.info("view permission is: " + viewPermit)
+		//page.removeContentPermissionSet(viewPerms)
+    
+		// Check if the page has edit permission
+		ContentPermissionSet editPermit = page
+                    .getContentPermissionSet(ContentPermission.EDIT_PERMISSION);
+		log.info("edit permission is: " + editPermit)
+		//page.removeContentPermissionSet(editPerms)
+    
+		// Set the view permission
+		if (viewPermit == null) {
+			def permissionManager = ComponentLocator.getComponent(ContentPermissionManager)
+			def editRestriction = ContentPermission.createGroupPermission(ContentPermission.VIEW_PERMISSION,"group-name")
+			permissionManager.addContentPermission(editRestriction, page)
+		}
+	}
+}
