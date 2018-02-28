c7ce3cd38d380c81176280644108d13c530f7320
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-27 20:35:09 -0500

    Import logger, and define page/space manager

--- groovy/page permission/update-all-space-page-permission.groovy
+++ groovy/page permission/update-all-space-page-permission.groovy
@@ -4,3 +4,9 @@ import com.atlassian.confluence.spaces.SpaceManager
 import com.atlassian.sal.api.component.ComponentLocator
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+
+def log = Logger.getLogger("com.onresolve.jira.groovy")
+log.setLevel(Level.INFO)
+
+def spaceManager = ComponentLocator.getComponent(SpaceManager)
+def pageManager = ComponentLocator.getComponent(PageManager)
