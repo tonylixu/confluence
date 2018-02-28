83254713eb0e4b054b2bd154f2cf078dacf01b79
tonylixu@gmail.com <tonylixu@gmail.com>
2018-02-27 20:34:33 -0500

    Created update-all-space-page restriction groovy file

---
+++ groovy/page permission/update-all-space-page-permission.groovy
@@ -0,0 +1,6 @@
+import com.atlassian.confluence.pages.Page
+import com.atlassian.confluence.pages.PageManager
+import com.atlassian.confluence.spaces.SpaceManager
+import com.atlassian.sal.api.component.ComponentLocator
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
